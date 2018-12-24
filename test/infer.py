f81e75793ddf690763a5dc9639d8dfbbd13851a0
peizhilin <wopeizl@163.com>
2018-12-24 10:47:07 +0800

    add this for test purpose

---
+++ test/infer.py
@@ -0,0 +1,42 @@
+#encoding:utf-8
+
+import paddle.fluid as fluid
+from PIL import Image
+import config as cfg
+import numpy as np
+
+# 获取执行器
+place = fluid.CPUPlace()
+exe = fluid.Executor(place)
+
+# 从保存的模型文件中获取预测程序、输入数据的名称和分类器
+[infer_program, feeded_var_names, target_vars] = fluid.io.load_inference_model(dirname='./model', executor=exe)
+
+
+# 对图片进行预处理
+def load_image(path):
+    img = Image.open(path)
+    # 统一图像大小
+    img = img.resize((cfg.READER.CROP_SIZE, cfg.READER.CROP_SIZE), Image.ANTIALIAS)
+    # 转换成numpy值
+    img = np.array(img).astype(np.float32)
+    # 转换成CHW
+    img = img.transpose((2, 0, 1))
+    # 转换成BGR
+    img = img[(2, 1, 0), :, :] / 255.0
+    return img
+
+
+# 添加待预测的图片
+infer_data = []
+infer_data.append(load_image('./dataset/cucumber/2e10432c-01ea-11e9-a2e1-c8ff285a4318.jpg'))
+infer_data.append(load_image('./dataset/apple/58d14a90-01e2-11e9-9470-c8ff285a4318.jpg'))
+infer_data = np.array(infer_data)
+print(infer_data.shape)
+
+results = exe.run(program=infer_program,
+                  feed={feeded_var_names[0]: infer_data},
+                  fetch_list=target_vars)
+print(results)
+lab = np.argsort(results)
+print(lab[0])
---
+++ test/train.py
@@ -0,0 +1,71 @@
+#encoding:utf-8
+
+import os
+import mobilenet_v2
+import reader
+import paddle.fluid as fluid
+import paddle
+import config as cfg
+
+# 获取分类器
+image = fluid.layers.data(name='image', shape=[cfg.TRAIN.IMAGE_CHANNEL, cfg.TRAIN.IMAGE_WIDTH, cfg.TRAIN.IMAGE_HEIGHT], dtype='float32')
+net = mobilenet_v2.mobilenet_v2(input=image, class_dim=cfg.TRAIN.CLASS_DIM, scale=1.0)
+
+# 获取损失函数和准确率函数
+label = fluid.layers.data(name='label', shape=[1], dtype='int64')
+cost = fluid.layers.cross_entropy(input=net, label=label)
+avg_cost = fluid.layers.mean(cost)
+accuracy = fluid.layers.accuracy(input=net, label=label)
+
+# 克隆测试程序
+test_program = fluid.default_main_program().clone(for_test=True)
+
+# 定义优化方法
+optimizer = fluid.optimizer.AdamOptimizer(learning_rate=cfg.TRAIN.LEARNING_RATE)
+opt = optimizer.minimize(avg_cost)
+
+# 获取执行器并进行初始化
+place = fluid.CUDAPlace(0) if cfg.TRAIN.USE_GPU else fluid.CPUPlace()
+exe = fluid.Executor(place)
+exe.run(fluid.default_startup_program())
+
+# 获取训练和测试数据
+train_reader = paddle.batch(reader=paddle.reader.shuffle(
+    reader=reader.train_reader(cfg.TRAIN.TRAIN_LIST), buf_size=20000), batch_size=cfg.TRAIN.BATCH_SIZE)
+test_reader = paddle.batch(reader=reader.test_reader(cfg.TRAIN.TEST_LIST), batch_size=cfg.TRAIN.BATCH_SIZE)
+
+# 定义输入数据维度
+feeder = fluid.DataFeeder(feed_list=[image, label], place=place)
+
+# 开始训练
+for pass_id in range(cfg.TRAIN.PASS_SUM):
+    # 训练
+    for batch_id, data in enumerate(train_reader()):
+        train_cost, train_acc = exe.run(program=fluid.default_main_program(),
+                                        feed=feeder.feed(data),
+                                        fetch_list=[avg_cost, accuracy])
+
+        if batch_id % 100 == 0:
+            print("Pass:%d, Batch:%d, Cost:%f, Accuracy:%f" % (pass_id, batch_id, train_cost[0], train_acc[0]))
+
+    # 测试
+    test_costs = []
+    test_accs = []
+    for batch_id, data in enumerate(test_reader()):
+        test_cost, test_acc = exe.run(program=test_program,
+                                      feed=feeder.feed(data),
+                                      fetch_list=[avg_cost, accuracy])
+        test_costs.append(test_cost[0])
+        test_accs.append(test_acc[0])
+    test_cost = sum(test_costs) / len(test_costs)
+    test_acc = sum(test_accs) / len(test_accs)
+    print("Test:%d, Cost:%f, Accuracy:%f" % (pass_id, test_cost, test_acc))
+
+    # 保存模型
+    save_model_path = './model/%d' % pass_id
+    if not os.path.exists(save_model_path):
+        os.makedirs(save_model_path)
+    fluid.io.save_inference_model(dirname=save_model_path,
+                                  feeded_var_names=[image.name],
+                                  target_vars=[net],
+                                  executor=exe)
